fd87515d363dd67b8810a92b93a2265ec716e5eb
Sadman Wasee <sadman.wasee125@gmail.com>
2026-05-02 15:19:09 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from datetime import date
+from utils import add
+from utils import subtract  
 
 # Get the current date
 today = date.today()
@@ -7,3 +9,6 @@ today = date.today()
 
 print("Sadman Wasee")
 print("Today's date:", today)
+
+add(2,3)
+subtract(10,7)
