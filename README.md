7a94b7d046ba9a51ad81f4a2e656419f0979cfcf
Sadman Wasee <sadman.wasee125@gmail.com>
2026-05-02 14:51:01 +0600

    Initial commit: project structure and basic program

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cataract Detection using CNN
+ 
+<p>Sadman Wasee</p>
+<p>This project detects the presence of cataracts in eye images</p> 
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+# Get the current date
+today = date.today()
+
+
+
+print("Sadman Wasee")
+print("Today's date:", today)
